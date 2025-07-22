fd72f9a2b530871ea1b8d85d00842153f47dfc8c
kelzerock <kelzzzer@gmail.com>
2025-07-22 09:10:48 +0300

    refactor: convert CrashComponent to functional component

--- src/components/CrashComponent.tsx
+++ src/components/CrashComponent.tsx
@@ -1,9 +1,3 @@
-import { Component } from "react";
-import type { PropsAbsent } from "../models/types/propsAbsent";
-
-export class CrashComponent extends Component<PropsAbsent> {
-  constructor(props: PropsAbsent) {
-    super(props);
-    throw new Error("Simulate error!");
-  }
-}
+export const CrashComponent = () => {
+  throw new Error("Simulate error!");
+};

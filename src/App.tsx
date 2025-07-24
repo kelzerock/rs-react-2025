17c3a1508dd927f9f14416b8f811c5f070e6c189
kelzerock <kelzzzer@gmail.com>
2025-07-24 22:43:26 +0300

    refactor: about page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="container mx-auto p-3 flex flex-col gap-3 h-full">
       <ErrorBoundary>
-        <div className="bg-blue-100 rounded-2xl p-3 flex flex-col gap-2 grow">
+        <div className="bg-blue-50 rounded-2xl p-3 flex flex-col gap-2 grow">
           <NavigationBar />
           <Outlet />
         </div>

30670d7a512f570ceb72a94affec6586ce5d8919
kelzerock <kelzzzer@gmail.com>
2025-08-03 11:47:03 +0300

    fix: title html

--- src/components/FlyOutPanel.tsx
+++ src/components/FlyOutPanel.tsx
@@ -1,17 +1,21 @@
 import { useAppDispatch, useAppSelector } from "../hooks/appHook";
 import { removeAllItems } from "../store/itemsSlice";
+import { downloadCSVFile } from "../utils/downloadCVSFile";
 
 export const FlyOutPanel = () => {
   const items = useAppSelector((state) => state.items);
   const dispatch = useAppDispatch();
+  const handleClickDownload = () => {
+    downloadCSVFile(items);
+  };
 
   const countItems = items.length;
   if (countItems > 0)
     return (
-      <div className=" bg-stone-700 p-2 text-stone-100 font-semibold rounded-md w-full sticky bottom-0 col-span-full">
+      <div className=" bg-stone-700/80 p-2 text-stone-100 font-semibold rounded-md w-full sticky bottom-3 col-span-full">
         <h6>Information about favorite items:</h6>
         <span>Selected items: {countItems}</span>
-        <div className="flex gap-3">
+        <div className="flex flex-col sm:flex-row gap-3">
           <button
             type="button"
             onClick={() => dispatch(removeAllItems())}
@@ -22,6 +26,7 @@ export const FlyOutPanel = () => {
           <button
             type="button"
             className="bg-stone-200 text-stone-800 px-3 rounded-md hover:cursor-pointer hover:bg-stone-400 transition-all duration-300 hover:text-stone-50"
+            onClick={handleClickDownload}
           >
             save selected items
           </button>

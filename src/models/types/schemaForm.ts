de82f65cb0a36aeeb7367d3ec1ba1caa1b5ee975
kelzerock <kelzzzer@gmail.com>
2025-08-27 21:53:56 +0300

    fix: bug for build

--- src/models/types/schemaForm.ts
+++
@@ -1,4 +0,0 @@
-import z from "zod";
-import type { SchemaForm } from "../../schema/schemaForm";
-
-export type SchemaFormType = z.infer<typeof SchemaForm>;
--- src/models/types/schemaFormForDb.ts
+++
@@ -1,6 +0,0 @@
-import type { SchemaFormType } from "./schemaForm";
-
-export type SchemaFormTypeForDb = Omit<SchemaFormType, "picture"> & {
-  picture: string;
-  id?: string;
-};

8fb23c977624c18a5d0a171ad4e3a80c40c1d9ce
kelzerock <kelzzzer@gmail.com>
2025-08-31 19:56:25 +0300

    feat: complete sort for fields

--- src/components/TableCountries.tsx
+++ src/components/TableCountries.tsx
@@ -9,6 +9,8 @@ import {
   addCountriesName,
   selectCountriesName,
 } from "../store/countriesNameSlice";
+import type { Country } from "../models/types/country";
+import { SortingPanel } from "./SortingPanel";
 
 let countriesPromise: Promise<Countries> | null = null;
 
@@ -40,6 +42,10 @@ const TableCountries = () => {
     gas_co2: false,
   });
   const [countryChoice, setCountryChoice] = useState("");
+  const [sortingPanelTitle, setSortingPanelTitle] = useState<{
+    title: string;
+    direction: "asc" | "desc";
+  }>({ title: "", direction: "asc" });
   const [year, setYear] = useState({
     choice: 2023,
     dateTime: new Date().getTime(),
@@ -101,7 +107,63 @@ const TableCountries = () => {
 
   const additionalCols = Object.values(moreInfoSet).filter(Boolean).length;
 
-  console.log({ countries });
+  const handleClickSortButton = (
+    target: string,
+    direction: "asc" | "desc",
+    typeData: "number" | "string",
+  ) => {
+    if (!filteredCountries) return;
+    const sortedCountries = Object.entries(filteredCountries).sort((a, b) => {
+      if (target === "country") {
+        const aValue = a[0];
+        const bValue = b[0];
+        return direction === "asc"
+          ? aValue.localeCompare(bValue)
+          : bValue.localeCompare(aValue);
+      }
+
+      if (target === "iso_code") {
+        const aValue = a[1]?.iso_code ?? "";
+        const bValue = b[1]?.iso_code ?? "";
+        return direction === "asc"
+          ? aValue.localeCompare(bValue)
+          : bValue.localeCompare(aValue);
+      }
+
+      const getValue = (
+        countryData: Country,
+        typeData: "number" | "string",
+      ) => {
+        const data = countryData?.data?.find((d) => d?.year === year.choice);
+        if (!data) return typeData === "number" ? 0 : "";
+        return (
+          data[target as keyof typeof data] ?? (typeData === "number" ? 0 : "")
+        );
+      };
+
+      const aValue = getValue(a[1], typeData);
+      const bValue = getValue(b[1], typeData);
+
+      if (
+        typeData === "number" &&
+        typeof aValue === "number" &&
+        typeof bValue === "number"
+      ) {
+        return direction === "asc" ? aValue - bValue : bValue - aValue;
+      }
+      if (
+        typeData === "string" &&
+        typeof aValue === "string" &&
+        typeof bValue === "string"
+      ) {
+        return direction === "asc"
+          ? aValue.localeCompare(bValue)
+          : bValue.localeCompare(aValue);
+      }
+      return 0;
+    });
+    setFilteredCountries(Object.fromEntries(sortedCountries));
+  };
 
   return (
     <>
@@ -121,8 +183,15 @@ const TableCountries = () => {
         <div
           className={`grid grid-cols-${Math.min(6 + additionalCols, 16)} border-b border-stone-400 sticky top-0 bg-stone-300`}
         >
-          <span className="col-span-1 flex flex-col gap-1 items-center p-0.5">
+          <div className="col-span-1 flex flex-col gap-1 items-center p-0.5">
             <label htmlFor="countries-name">Country</label>
+            <SortingPanel
+              handleClickSortButton={handleClickSortButton}
+              title="country"
+              typeData="string"
+              sortingPanelTitle={sortingPanelTitle}
+              setSortingPanelTitle={(value) => setSortingPanelTitle(value)}
+            />
             <input
               className="w-full border border-stone-400 rounded-md p-1 focus:outline-none"
               type="text"
@@ -138,9 +207,18 @@ const TableCountries = () => {
                 </option>
               ))}
             </datalist>
-          </span>
-          <span className="col-span-1">iso_code</span>
-          <span className="col-span-1">
+          </div>
+          <div className="col-span-1">
+            iso_code
+            <SortingPanel
+              handleClickSortButton={handleClickSortButton}
+              title="iso_code"
+              typeData="string"
+              sortingPanelTitle={sortingPanelTitle}
+              setSortingPanelTitle={(value) => setSortingPanelTitle(value)}
+            />
+          </div>
+          <div className="col-span-1">
             <label htmlFor="year" className="flex gap-1">
               Year
               <select value={year.choice} onChange={handleChangeYear}>
@@ -151,17 +229,53 @@ const TableCountries = () => {
                 ))}
               </select>
             </label>
-          </span>
-          <span className="col-span-1">population</span>
-          <span className="col-span-1">co2</span>
-          <span className="col-span-1">co2_per_capita</span>
+          </div>
+          <div className="col-span-1 flex gap-1 items-center">
+            population
+            <SortingPanel
+              handleClickSortButton={handleClickSortButton}
+              title="population"
+              typeData="number"
+              sortingPanelTitle={sortingPanelTitle}
+              setSortingPanelTitle={(value) => setSortingPanelTitle(value)}
+            />
+          </div>
+          <div className="col-span-1">
+            co2
+            <SortingPanel
+              handleClickSortButton={handleClickSortButton}
+              title="co2"
+              typeData="number"
+              sortingPanelTitle={sortingPanelTitle}
+              setSortingPanelTitle={(value) => setSortingPanelTitle(value)}
+            />
+          </div>
+          <div className="col-span-1">
+            co2_per_capita
+            <SortingPanel
+              handleClickSortButton={handleClickSortButton}
+              title="co2_per_capita"
+              typeData="number"
+              sortingPanelTitle={sortingPanelTitle}
+              setSortingPanelTitle={(value) => setSortingPanelTitle(value)}
+            />
+          </div>
           {additionalCols > 0 &&
             Object.entries(moreInfoSet).map(
               ([key, value]) =>
                 value && (
-                  <span className="col-span-1" key={key}>
+                  <div className="col-span-1" key={key}>
                     {key}
-                  </span>
+                    <SortingPanel
+                      handleClickSortButton={handleClickSortButton}
+                      title={key}
+                      typeData="number"
+                      sortingPanelTitle={sortingPanelTitle}
+                      setSortingPanelTitle={(value) =>
+                        setSortingPanelTitle(value)
+                      }
+                    />
+                  </div>
                 ),
             )}
         </div>
